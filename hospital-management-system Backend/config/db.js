48090113c3ec47b42430e4397e6e6fa7c04ea8c4
Shovin Michel David <149549372+shovin12leo@users.noreply.github.com>
2025-02-01 10:33:33 +0530

    Add files via upload

--- hospital-management-system Backend/config/db.js
+++ hospital-management-system Backend/config/db.js
@@ -1,1 +1,9 @@
+const mongoose = require('mongoose');
 
+mongoose.connect('mongodb://127.0.0.1:27017/hms_db')
+    .then(() => {
+        console.log('MongoDB connected');
+    })
+    .catch((err) => {
+        console.error('MongoDB connection error:', err);
+    });
